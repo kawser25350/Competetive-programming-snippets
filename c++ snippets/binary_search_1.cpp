f7e470c0ecb80eb6a103656d3aa0d936ade64123
Kawser Ahmmed <136078973+kawser25350@users.noreply.github.com>
2024-12-25 15:08:06 +0600

    Update binary_search_1.cpp

--- c++ snippets/binary_search_1.cpp
+++ c++ snippets/binary_search_1.cpp
@@ -1,1 +1,21 @@
+int index= lower_bound(v.begin(),v.end()) - v.begin(); //return same value's  or lower's index
+int index = upper_bound(v.begin(),v.end()) - v.begin(); //return upper values's index all time 
 
+   int l=0,r=1e18,ans,mid;
+
+   while(l<=r){
+
+      mid=l+(r-l)/2;
+
+      if(ok(mid)){
+
+         r=mid-1;
+
+         ans=mid;
+
+      }
+      else{
+
+         l=mid+1;
+      }
+   }

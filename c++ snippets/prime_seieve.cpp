7d7ba07b0c31c228b0b7c8e4933324c2a2c698aa
Kawser Ahmmed <136078973+kawser25350@users.noreply.github.com>
2024-12-25 15:14:07 +0600

    Update prime_seieve.cpp

--- c++ snippets/prime_seieve.cpp
+++ c++ snippets/prime_seieve.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
 void primesieve(int n, vector<int>& pr) {
     vector<bool> prime(n + 1, true);
-    memset(prime.data(), true, prime.size());
 
     for (int p = 2; p * p <= n; p++) {
-        if (prime[p] == true) {
+        if (prime[p]) {
             for (int i = p * p; i <= n; i += p) {
                 prime[i] = false;
             }
@@ -15,4 +20,21 @@ void primesieve(int n, vector<int>& pr) {
             pr.push_back(p);
         }
     }
+}
+
+int main() {
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    vector<int> primes;
+    primesieve(n, primes);
+
+    cout << "Prime numbers up to " << n << " are: ";
+    for (int prime : primes) {
+        cout << prime << " ";
+    }
+    cout << endl;
+
+    return 0;
 }

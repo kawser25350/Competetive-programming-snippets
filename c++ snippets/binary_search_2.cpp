17d3aa61b6df2eaf359cb18c255c74e4f77dc6d0
Kawser Ahmmed <136078973+kawser25350@users.noreply.github.com>
2024-12-25 15:09:28 +0600

    Update binary_search_2.cpp

--- c++ snippets/binary_search_2.cpp
+++ c++ snippets/binary_search_2.cpp
@@ -1,1 +1,35 @@
 
+
+// greedy or any operaion  ( when found monotonic beaheahiner
+ auto ok=[&](long long mid)
+    {
+        int total=0;
+        for (int i = 0;i < n;i++) {
+            total += (mid / v[i]);
+            if (total >= t) {
+                return true;
+            }
+        }
+        return false;
+        };
+
+
+   int l=0,r=1e18,ans,mid;
+
+   while(l<=r){
+
+      mid=l+(r-l)/2;
+
+      if(ok(mid)){
+
+         r=mid-1;
+
+         ans=mid;
+
+      }
+      else{
+
+         l=mid+1;
+      }
+   }
+
